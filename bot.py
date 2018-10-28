c90d7812090fbb018826fe28b4817165cf107a83
projectinfob <43008234+projectinfob@users.noreply.github.com>
2018-10-28 09:56:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,8 +13,7 @@ bot=telebot.TeleBot(os.environ['TELEGRAM_TOKEN'])
 
 @bot.message_handler()
 def handl(m):
-   prices=[LabeledPrice(label='За хуй в рот',amount=100),LabeledPrice(label='За залупу в рот',amount=350)
-   
+   prices=[LabeledPrice(label='За хуй в рот',amount=100),LabeledPrice(label='За залупу в рот',amount=350)]
    bot.send_invoice(m.chat.id, 'Покупай хуету!','Описание к хуете','payload1', '4242 4242 4242 4242','startparametr','RUB',prices)
 
 if True:
