081c96f892221a486a970b5b4c0f008480434efa
projectinfob <43008234+projectinfob@users.noreply.github.com>
2018-10-28 10:09:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,9 +15,9 @@ bot=telebot.TeleBot(os.environ['TELEGRAM_TOKEN'])
 
 @bot.message_handler()
 def handl(m):
-   prices1=[LabeledPrice(label='За хуй в рот',amount=100),LabeledPrice(label='За залупу в рот',amount=350)]
-   bot.send_invoice(m.chat.id, title='Покупай хуету!',description='Описание к хуете',\
-                    provider_token='381764678:TEST:4376',currency='RUB',prices=prices1,invoice_payload='payload',start_parameter='startp')
+   prices1=[LabeledPrice(label='За хуй в рот',amount=100),LabeledPrice(label='За доставку на дом',amount=350)]
+   bot.send_invoice(m.chat.id, title='Покупай хуй в рот!',description='Описание к хую',\
+                    provider_token='381764678:TEST:7232',currency='RUB',prices=prices1,invoice_payload='payload',start_parameter='startp')
 
 if True:
    print('bot is working')
